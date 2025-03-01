403774e0cff88fc5a12830955a5dfb41f496de9d
Karthik Rathinavelu <r.karthik@outlook.com>
2025-03-01 22:27:03 +0530

    More settings and options for Neovim

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,14 +6,15 @@ vim.opt.mouse = "a"                    -- enable mouse mode, can be useful for r
 
 vim.opt.showmode = false               -- Don't show mode, since it's already part of status line
 
-vim.opt.smartindent = true
-vim.opt.autoindent = true
+vim.opt.smartindent = true             -- Do smart auto indenting when starting new line
+vim.opt.autoindent = true              -- It should be on when smartindent is on
 vim.opt.breakindent = true             -- Break indent for long lines so that they look visually indented
 
 vim.opt.undofile = true                -- Save undo history
 
 vim.opt.ignorecase = true              -- Case insensitive search
 vim.opt.smartcase = true               -- unless capital letters are in the search term
+vim.opt.hlsearch = true                -- highlight the search term
 
 vim.opt.signcolumn = "yes"             -- Keep signcolumn on by default, useful in LSP and Git
 
@@ -36,6 +37,22 @@ vim.opt.shiftwidth = 4
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.smarttab = true
+
+-- :h fo-table. Format options
+vim.opt.formatoptions = {}
+vim.opt.formatoptions = {
+    ["1"] = true, -- Don't break after one letter word
+    ["2"] = true, -- Use indent from 2nd line of a paragraph
+    q = true,     -- Continue comments with gq"
+    n = true,     -- Recognize numbered list
+    l = true,     -- Don't format when a line is alredy longer than textwidth when insert started
+    j = true,     -- Remove comment leader when joining lines
+    v = true,     -- Only break at a blank during the current insert command
+    r = false,    -- Don't continue comments when pressing enter
+    o = false,    -- Don't insert comment leader during o or O in normal mode
+    c = false,    -- Don't auto-wrap comments using textwidth
+    t = false,    -- Don't auto-wrap text using textwidth
+}
 
 -- How splits should be opened
 vim.opt.splitright = true
