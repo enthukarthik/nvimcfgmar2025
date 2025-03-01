0a1434e65d988026b0cc6342ef50881132735d59
Karthik Rathinavelu <r.karthik@outlook.com>
2025-03-01 22:40:19 +0530

    Changing indentation for all files. Choosing ayu-dark as the default for now

--- lua/plugins/colorschemes.lua
+++ lua/plugins/colorschemes.lua
@@ -1,102 +1,96 @@
 return {
-	{
-		"ellisonleao/gruvbox.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		"sainnhe/gruvbox-material",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		-- tokyonight themes in the order of bright to dark
-		-- tokyonight-day, tokyonight-storm, tokyonight-moon, tokyonight-night
-		"folke/tokyonight.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		"tiagovla/tokyodark.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		-- catppuccin themes in the order of bright to dark
-		-- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
-		"catppuccin/nvim", 
-		lazy = true,
-		name = "catppuccin", 
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		"rebelot/kanagawa.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		"navarasu/onedark.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = {
-			style = "darker",
-			toggle_style_key = "<leader>ts",
-			toggle_style_list = {"dark","darker","cool","deep","warm","warmer","light"},
-		},
-		config = function(_, opts)
-			require("onedark").setup(opts)
-		end,
-	},
-	{
-		-- onelight, onedark, onedark_vivid, onedark_dark
-		"olimorris/onedarkpro.nvim",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		"sainnhe/sonokai",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		"tomasr/molokai",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
-	{
-		-- ayu-light, ayu-mirage, ayu-dark
-		"Shatur/neovim-ayu",
-		lazy = true,
-		priority = 1000,
-		opts = {},
-		config = function(_, opts)
-		end,
-	},
+    {
+        -- ayu-light, ayu-mirage, ayu-dark
+        "Shatur/neovim-ayu",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+            vim.cmd.colorscheme("ayu-dark")
+        end,
+    },
+    {
+        "ellisonleao/gruvbox.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+            vim.cmd.colorscheme("gruvbox")
+        end,
+    },
+    {
+        -- tokyonight themes in the order of bright to dark
+        -- tokyonight-day, tokyonight-storm, tokyonight-moon, tokyonight-night
+        "folke/tokyonight.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+        end,
+    },
+    {
+        "tiagovla/tokyodark.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+        end,
+    },
+    {
+        -- catppuccin themes in the order of bright to dark
+        -- catppuccin-latte, catppuccin-frappe, catppuccin-macchiato, catppuccin-mocha
+        "catppuccin/nvim", 
+        lazy = true,
+        name = "catppuccin", 
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+        end,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+        end,
+    },
+    {
+        "navarasu/onedark.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = {
+            style = "darker",
+            toggle_style_key = "<leader>ts",
+            toggle_style_list = {"dark","darker","cool","deep","warm","warmer","light"},
+        },
+        config = function(_, opts)
+            require("onedark").setup(opts)
+        end,
+    },
+    {
+        -- onelight, onedark, onedark_vivid, onedark_dark
+        "olimorris/onedarkpro.nvim",
+        lazy = true,
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+        end,
+    },
+    {
+        "sainnhe/sonokai",
+        lazy = true,
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+        end,
+    },
+    {
+        "tomasr/molokai",
+        lazy = true,
+        priority = 1000,
+        opts = {},
+        config = function(_, opts)
+        end,
+    },
 }
--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,16 +1,16 @@
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-		config = function()
-			-- mini.statusline related
-			require("mini.icons").setup()		-- enable icons
-			require("mini.git").setup()		-- enable git info
-			require("mini.diff").setup()		-- enable git diff info
-			require("mini.statusline").setup()	-- enable mini statusline
+    {
+        "echasnovski/mini.nvim",
+        version = false,
+        config = function()
+            -- mini.statusline related
+            require("mini.icons").setup()       -- enable icons
+            require("mini.git").setup()         -- enable git info
+            require("mini.diff").setup()        -- enable git diff info
+            require("mini.statusline").setup()  -- enable mini statusline
 
-			-- mini.files setup
-			require("mini.files").setup()		-- enable filesystem navigation through buffer
-		end,
-	},
+            -- mini.files setup
+            require("mini.files").setup()       -- enable filesystem navigation through buffer
+        end,
+    },
 }
