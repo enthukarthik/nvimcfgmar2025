4f45d6ca27dca15f93915098db8165899a72fa12
Karthik Rathinavelu <r.karthik@outlook.com>
2025-03-02 09:18:11 +0530

    Showkeys plugin to showcase key presses, if required

---
+++ lua/plugins/showkeys.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "nvzone/showkeys",
+        cmd = "ShowkeysToggle",
+        opts = {
+            timeout = 2,
+            maxkeys = 5,
+            show_count = true,
+            excluded_modes = { "i" }
+        }
+    }
+}
+
