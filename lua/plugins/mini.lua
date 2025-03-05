4126e5f823f98d73e9bce1a042e457938c948ab8
Karthik Rathinavelu <r.karthik@outlook.com>
2025-03-05 13:31:37 +0530

    Enabling which-key and modifying mini.clue

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -49,35 +49,35 @@ return {
             local miniclue = require("mini.clue");
             miniclue.setup({                   -- cute prompts about bindings
                 triggers = {
-                    { mode = "n", keys = "<Leader>" },
-                    { mode = "x", keys = "<Leader>" },
-
-                    -- Built-in completion
-                    { mode = "i", keys = "<C-x>" },
-
-                    -- `g` key
-                    { mode = "n", keys = "g" },
-                    { mode = "x", keys = "g" },
-
-                    -- Marks
-                    { mode = "n", keys = "'" },
-                    { mode = "n", keys = "`" },
-                    { mode = "x", keys = "'" },
-                    { mode = "x", keys = "`" },
-
-                    -- Registers
-                    { mode = "n", keys = '"' },
-                    { mode = "x", keys = '"' },
-                    { mode = "i", keys = "<C-r>" },
-                    { mode = "c", keys = "<C-r>" },
-
-                    -- Window commands
-                    { mode = "n", keys = "<C-w>" },
-
-                    -- `z` key for folds
-                    { mode = "n", keys = "z" },
-                    { mode = "x", keys = "z" },
-
+            --         { mode = "n", keys = "<Leader>" },
+            --         { mode = "x", keys = "<Leader>" },
+            --
+            --         -- Built-in completion
+            --         { mode = "i", keys = "<C-x>" },
+            --
+            --         -- `g` key
+            --         { mode = "n", keys = "g" },
+            --         { mode = "x", keys = "g" },
+            --
+            --         -- Marks
+            --         { mode = "n", keys = "'" },
+            --         { mode = "n", keys = "`" },
+            --         { mode = "x", keys = "'" },
+            --         { mode = "x", keys = "`" },
+            --
+            --         -- Registers
+            --         { mode = "n", keys = '"' },
+            --         { mode = "x", keys = '"' },
+            --         { mode = "i", keys = "<C-r>" },
+            --         { mode = "c", keys = "<C-r>" },
+            --
+            --         -- Window commands
+            --         { mode = "n", keys = "<C-w>" },
+            --
+            --         -- `z` key for folds
+            --         { mode = "n", keys = "z" },
+            --         { mode = "x", keys = "z" },
+            --
                     -- Bracketed
                     { mode = "n", keys = "[" },
                     { mode = "n", keys = "]" },
